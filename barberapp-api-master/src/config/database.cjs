5172b03a872e6c1fc7068321b6de099136cd020a
Yago Brito <yc.brito10@gmail.com>
2025-12-08 22:13:32 -0300

    fix: adiciona configuração CommonJS para sequelize-cli

---
+++ barberapp-api-master/src/config/database.cjs
@@ -0,0 +1,28 @@
+require('dotenv/config');
+
+module.exports = {
+    dialect: 'postgres',
+    host: process.env.DB_HOST || '127.0.0.1',
+    port: process.env.DB_PORT || 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    dialectOptions: {
+        ssl: process.env.NODE_ENV === 'production' ? {
+            require: true,
+            rejectUnauthorized: false
+        } : false,
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+    logging: false,
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
